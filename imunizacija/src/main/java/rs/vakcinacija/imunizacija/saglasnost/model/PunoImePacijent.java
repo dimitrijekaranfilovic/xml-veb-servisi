98e96b8181b6d2aaaff29a9a91df3cdb2ae1e5f1
ivanmrsulja <nighteliteace@gmail.com>
2022-01-04 18:42:38 +0100

    Slight refactor, updated schema and generated document for vaccination agreement.

--- imunizacija/src/main/java/rs/vakcinacija/imunizacija/saglasnost/model/PunoImePacijent.java
+++ imunizacija/src/main/java/rs/vakcinacija/imunizacija/saglasnost/model/PunoImePacijent.java
@@ -1,43 +1,24 @@
 package rs.vakcinacija.imunizacija.saglasnost.model;
 
+import rs.vakcinacija.imunizacija.zajednicko.model.ImePrezime;
+
 import javax.xml.bind.annotation.*;
 
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlRootElement(name = "puno_ime")
-@XmlType(name = "TPunoIme", propOrder = {"ime", "prezime", "imeRoditelja"})
-public class PunoImePacijent {
-
-    @XmlElement(name = "ime", required = true, namespace = "https://www.vakcinacija.rs/zajednicko")
-    private String ime;
-
-    @XmlElement(name = "prezime", required = true, namespace = "https://www.vakcinacija.rs/zajednicko")
-    private String prezime;
+@XmlType(name = "TPunoIme", propOrder = {"imeRoditelja"})
+public class PunoImePacijent extends ImePrezime {
 
     @XmlElement(name = "ime_roditelja", required = true)
     private String imeRoditelja;
 
-    public PunoImePacijent() {}
+    public PunoImePacijent() {
+        super();
+    }
 
     public PunoImePacijent(String ime, String prezime, String imeRoditelja) {
-        this.ime = ime;
-        this.prezime = prezime;
+        super(ime, prezime);
         this.imeRoditelja = imeRoditelja;
-    }
-
-    public String getIme() {
-        return ime;
-    }
-
-    public void setIme(String ime) {
-        this.ime = ime;
-    }
-
-    public String getPrezime() {
-        return prezime;
-    }
-
-    public void setPrezime(String prezime) {
-        this.prezime = prezime;
     }
 
     public String getImeRoditelja() {
