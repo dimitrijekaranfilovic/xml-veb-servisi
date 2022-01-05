8ff8f9d722d45824f56bcb35c1fb3734e843fd3b
ivanmrsulja <nighteliteace@gmail.com>
2022-01-05 17:56:43 +0100

    Added ExistDB marshalling and unmarshalling.

--- imunizacija/src/main/java/rs/vakcinacija/imunizacija/saglasnost/controller/SaglasnostController.java
+++ imunizacija/src/main/java/rs/vakcinacija/imunizacija/saglasnost/controller/SaglasnostController.java
@@ -1,13 +1,12 @@
 package rs.vakcinacija.imunizacija.saglasnost.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import rs.vakcinacija.imunizacija.saglasnost.model.SaglasnostZaSprovodjenjeImunizacije;
+import rs.vakcinacija.imunizacija.saglasnost.repository.SaglasnostRepository;
 
 @RestController
 @RequestMapping(value = "saglasnost",
@@ -15,8 +14,17 @@ import rs.vakcinacija.imunizacija.saglasnost.model.SaglasnostZaSprovodjenjeImuni
         consumes = MediaType.APPLICATION_XML_VALUE)
 public class SaglasnostController {
 
+    @Autowired
+    SaglasnostRepository saglasnostRepository;
+
     @PostMapping
-    public ResponseEntity<SaglasnostZaSprovodjenjeImunizacije> test(@RequestBody SaglasnostZaSprovodjenjeImunizacije saglasnost) {
+    public ResponseEntity<SaglasnostZaSprovodjenjeImunizacije> testWrite(@RequestBody SaglasnostZaSprovodjenjeImunizacije saglasnost) throws Exception {
+        saglasnostRepository.save(saglasnost);
         return new ResponseEntity<>(saglasnost, HttpStatus.CREATED);
+    }
+
+    @GetMapping(value = "/{id}", consumes = MediaType.ALL_VALUE)
+    public ResponseEntity<SaglasnostZaSprovodjenjeImunizacije> testRead(@PathVariable String id) throws Exception {
+        return new ResponseEntity<>(saglasnostRepository.retrieve(), HttpStatus.CREATED);
     }
 }
