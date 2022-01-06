bd226b634bff03dd658cbfb598c651a1817fb3a7
Dimitrije Karanfilovic <dimitrije.karanfilovic@gmail.com>
2022-01-06 14:29:32 +0100

    Added model classes to zajednicko project.

---
+++ zajednicko/src/main/java/model/Doza.java
@@ -0,0 +1,53 @@
+package model;
+
+import javax.xml.bind.annotation.*;
+import java.util.Date;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement(name = "doza_osnova")
+@XmlType(name = "TDoza", propOrder = {"brojDoze", "datumDavanja", "brojSerije"})
+public class Doza {
+
+    @XmlElement(name = "broj_doze", required = true)
+    private Integer brojDoze;
+
+    @XmlElement(name = "datum_davanja", required = true)
+    @XmlSchemaType(name = "date")
+    private Date datumDavanja;
+
+    @XmlElement(name = "broj_serije", required = true)
+    private String brojSerije;
+
+    public Doza() {
+    }
+
+    public Doza(Integer brojDoze, Date datumDavanja, String brojSerije) {
+        this.brojDoze = brojDoze;
+        this.datumDavanja = datumDavanja;
+        this.brojSerije = brojSerije;
+    }
+
+    public Integer getBrojDoze() {
+        return brojDoze;
+    }
+
+    public void setBrojDoze(Integer brojDoze) {
+        this.brojDoze = brojDoze;
+    }
+
+    public Date getDatumDavanja() {
+        return datumDavanja;
+    }
+
+    public void setDatumDavanja(Date datumDavanja) {
+        this.datumDavanja = datumDavanja;
+    }
+
+    public String getBrojSerije() {
+        return brojSerije;
+    }
+
+    public void setBrojSerije(String brojSerije) {
+        this.brojSerije = brojSerije;
+    }
+}
---
+++ zajednicko/src/main/java/model/package-info.java
@@ -0,0 +1,10 @@
+
+@XmlSchema(
+        namespace = "https://www.vakcinacija.rs/zajednicko",
+        elementFormDefault = XmlNsForm.QUALIFIED,
+        xmlns = {@javax.xml.bind.annotation.XmlNs(prefix = "za", namespaceURI = "https://www.vakcinacija.rs/zajednicko"),
+                @javax.xml.bind.annotation.XmlNs(prefix = "xs", namespaceURI = "http://www.w3.org/2001/XMLSchema")}
+)
+package model;
+import javax.xml.bind.annotation.XmlNsForm;
+import javax.xml.bind.annotation.XmlSchema;
