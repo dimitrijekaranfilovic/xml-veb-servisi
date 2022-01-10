38a13cdd7f661989a589354d043eff1d11ca7aa9
njmarko <marko.njegomir@uns.ac.rs>
2022-01-10 12:31:48 +0100

    Updated package info

--- imunizacija/src/main/java/rs/vakcinacija/imunizacija/interesovanje/model/package-info.java
+++ imunizacija/src/main/java/rs/vakcinacija/imunizacija/interesovanje/model/package-info.java
@@ -1,7 +1,9 @@
 @XmlSchema(
         namespace = "https://www.vakcinacija.rs/interesovanje",
         elementFormDefault = XmlNsForm.QUALIFIED,
-        xmlns = {@javax.xml.bind.annotation.XmlNs(prefix = "", namespaceURI = "https://www.vakcinacija.rs/interesovanje"),
+        xmlns = {@javax.xml.bind.annotation.XmlNs(prefix = "za", namespaceURI = "https://www.vakcinacija.rs/zajednicko"),
+                @javax.xml.bind.annotation.XmlNs(prefix = "pred", namespaceURI = "https://www.vakcinacija.rs/rdf/predicate/"),
+                @javax.xml.bind.annotation.XmlNs(prefix = "", namespaceURI = "https://www.vakcinacija.rs/interesovanje"),
                 @javax.xml.bind.annotation.XmlNs(prefix = "xs", namespaceURI = "http://www.w3.org/2001/XMLSchema")}
 )
 package rs.vakcinacija.imunizacija.interesovanje.model;
