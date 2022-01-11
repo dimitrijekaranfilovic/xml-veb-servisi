365617ab21e97f6ffb0ab02e72f0cb69d63499ad
ivanmrsulja <nighteliteace@gmail.com>
2022-01-11 14:00:12 +0100

    Refactored saglasnost service so it uses rdf builder, refactored model, fixed error handler so it catches RuntimeException instances.

--- imunizacija/src/main/java/rs/vakcinacija/imunizacija/saglasnost/model/Lekar.java
+++ imunizacija/src/main/java/rs/vakcinacija/imunizacija/saglasnost/model/Lekar.java
@@ -10,13 +10,10 @@ import java.util.UUID;
 
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlRootElement(name = "lekar")
-@XmlType(name = "TLekar", propOrder = {"id", "telefon"})
+@XmlType(name = "TLekar", propOrder = {"telefon"})
 @Getter
 @Setter
 public class Lekar extends ImePrezime {
-
-    @XmlElement(name = "id", required = false)
-    private UUID id;
 
     @XmlElement(name = "telefon", required = true)
     private Telefon telefon;
@@ -27,7 +24,6 @@ public class Lekar extends ImePrezime {
 
     public Lekar(RDFString ime, RDFString prezime, UUID id, Telefon telefon) {
         super(ime, prezime);
-        this.id = id;
         this.telefon = telefon;
     }
 }
