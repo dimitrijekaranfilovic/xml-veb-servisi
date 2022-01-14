fb87f6b28b3de81ab58ef9b02b249e12278a5b91
Dusan Erdeljan <dusanerdeljan99@gmail.com>
2022-01-14 11:17:00 +0100

    Fixed email service so that it accepts xml via rest endpoint.

---
+++ email-service/src/main/java/rs/vakcinacija/email/config/AppConfig.java
@@ -0,0 +1,15 @@
+package rs.vakcinacija.email.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class AppConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.defaultContentType(MediaType.valueOf(MediaType.APPLICATION_XML_VALUE));
+    }
+}
