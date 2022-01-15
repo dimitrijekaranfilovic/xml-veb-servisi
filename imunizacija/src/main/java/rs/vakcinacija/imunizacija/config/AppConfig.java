cdaeec76f0ef92f60e82cf5b57cda93ed1815a02
Ivan <nighteliteace@gmail.com>
2022-01-15 11:20:03 +0100

    Fixed communication over http.

--- imunizacija/src/main/java/rs/vakcinacija/imunizacija/config/AppConfig.java
+++ imunizacija/src/main/java/rs/vakcinacija/imunizacija/config/AppConfig.java
@@ -1,8 +1,12 @@
 package rs.vakcinacija.imunizacija.config;
 
 
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.xml.Jaxb2RootElementHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,5 +16,11 @@ public class AppConfig implements WebMvcConfigurer {
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
         configurer.defaultContentType(MediaType.valueOf(MediaType.APPLICATION_XML_VALUE));
+    }
+
+    @Bean
+    public HttpMessageConverters customConverters() {
+        HttpMessageConverter<?> additional = new Jaxb2RootElementHttpMessageConverter();
+        return new HttpMessageConverters(additional);
     }
 }
