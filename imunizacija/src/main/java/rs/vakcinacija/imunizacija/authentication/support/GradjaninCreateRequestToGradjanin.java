557f0f146c711c7843c8d798e7d22a103f48c2bb
Ivan <nighteliteace@gmail.com>
2022-02-12 20:55:11 +0100

    Set model mapper strategy strict for authorization.

--- imunizacija/src/main/java/rs/vakcinacija/imunizacija/authentication/support/GradjaninCreateRequestToGradjanin.java
+++ imunizacija/src/main/java/rs/vakcinacija/imunizacija/authentication/support/GradjaninCreateRequestToGradjanin.java
@@ -1,6 +1,7 @@
 package rs.vakcinacija.imunizacija.authentication.support;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.stereotype.Component;
 import rs.vakcinacija.imunizacija.authentication.dto.GradjaninCreateRequest;
 import rs.vakcinacija.imunizacija.authentication.model.Gradjanin;
@@ -15,6 +16,7 @@ public class GradjaninCreateRequestToGradjanin extends AbstractConverter<Gradjan
 
     @Override
     public Gradjanin convert(GradjaninCreateRequest gradjaninCreateRequest) {
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
         return modelMapper.map(gradjaninCreateRequest, Gradjanin.class);
     }
 }
