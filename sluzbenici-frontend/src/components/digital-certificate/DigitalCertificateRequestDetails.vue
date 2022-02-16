267f1f90fce301cd0ed9a230ba067db68616fe91
Dusan Erdeljan <dusanerdeljan99@gmail.com>
2022-02-16 15:41:20 +0100

    Added rich text field to digital certificate request view.

--- sluzbenici-frontend/src/components/digital-certificate/DigitalCertificateRequestDetails.vue
+++ sluzbenici-frontend/src/components/digital-certificate/DigitalCertificateRequestDetails.vue
@@ -2,58 +2,45 @@
   <v-list flat>
     <v-list-item>
       <v-list-item-content>
-        <v-list-item-title
-          >Име и презиме:
+        <v-list-item-title class="mb-2"
+          ><b>Подносилац захтева: </b></v-list-item-title
+        >
+        <p class="mb-2">
+          Име и презиме:
           {{ request.podnosilacZahteva.licniPodaci.ime | deRdf }}
           {{ request.podnosilacZahteva.licniPodaci.prezime | deRdf }}
-        </v-list-item-title>
+        </p>
+        <p class="mb-2">
+          Пол:
+          {{ request.podnosilacZahteva.licniPodaci.pol | deRdf }}
+        </p>
+        <p class="mb-2" v-if="request.podnosilacZahteva.licniPodaci.jmbg.value">
+          Јединствени матични број грађанина:
+          {{ request.podnosilacZahteva.licniPodaci.jmbg | deRdf }}
+        </p>
+        <p class="mb-2" v-if="request.podnosilacZahteva.brojPasosa.value">
+          Број пасоша:
+          {{ request.podnosilacZahteva.brojPasosa | deRdf }}
+        </p>
+        <p class="mb-2">
+          Адреса електронске поште:
+          {{ request.podnosilacZahteva.email | deRdf }}
+        </p>
       </v-list-item-content>
     </v-list-item>
 
     <v-list-item>
       <v-list-item-content>
-        <v-list-item-title
-          >Пол:
-          {{
-            request.podnosilacZahteva.licniPodaci.pol | deRdf
-          }}</v-list-item-title
+        <v-list-item-title class="mb-2"
+          ><b>Разлог за подношење захтева: </b></v-list-item-title
         >
-      </v-list-item-content>
-    </v-list-item>
-
-    <v-list-item>
-      <v-list-item-content>
-        <v-list-item-title
-          >Јединствени матични број грађанина:
-          {{
-            request.podnosilacZahteva.licniPodaci.jmbg | deRdf
-          }}</v-list-item-title
-        >
-      </v-list-item-content>
-    </v-list-item>
-
-    <v-list-item>
-      <v-list-item-content>
-        <v-list-item-title
-          >Број пасоша:
-          {{ request.podnosilacZahteva.brojPasosa | deRdf }}</v-list-item-title
-        >
-      </v-list-item-content>
-    </v-list-item>
-
-    <v-list-item>
-      <v-list-item-content>
-        <v-list-item-title
-          >Адреса електронске поште:
-          {{ request.podnosilacZahteva.email | deRdf }}</v-list-item-title
-        >
-      </v-list-item-content>
-    </v-list-item>
-
-    <v-list-item>
-      <v-list-item-content>
-        <v-list-item-title>Разлог за подношење захтева:</v-list-item-title>
-        {{ request.razlogZaPodnosenjeZahteva | deRdf }}
+        <quill-editor
+          class="editor"
+          ref="myTextEditor"
+          v-model="request.razlogZaPodnosenjeZahteva.value"
+          :options="editorOption"
+          :disabled="true"
+        />
       </v-list-item-content>
     </v-list-item>
   </v-list>
@@ -61,8 +48,25 @@
 
 <script>
 import Vue from "vue";
+import { quillEditor } from "vue-quill-editor";
+// import theme style
+import "quill/dist/quill.core.css";
+import "quill/dist/quill.snow.css";
+
 export default Vue.extend({
   name: "DigitalCertificateRequestDetails",
+  components: { quillEditor },
   props: ["request"],
+  data: () => ({
+    editorOption: {
+      placeholder: "Разлог за подношење захтева...",
+      modules: {
+        toolbar: false,
+        syntax: {
+          highlight: (text) => hljs.highlightAuto(text).value,
+        },
+      },
+    },
+  }),
 });
 </script>
