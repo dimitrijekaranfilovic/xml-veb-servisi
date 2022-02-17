363dc0cb83a28a40c83ed212b92b9c232b91f852
njmarko <marko.njegomir@uns.ac.rs>
2022-02-17 19:23:18 +0100

    Updated options for Rich text component in Zahtev za sertifikat form.

--- imunizacija-frontend/src/components/zahtevZaSertifikat/ZahtevZaSertifikatForm.vue
+++ imunizacija-frontend/src/components/zahtevZaSertifikat/ZahtevZaSertifikatForm.vue
@@ -135,21 +135,7 @@ export default {
     editorOption: {
       placeholder: "Унесите разлог за подношење захтева овде...",
       modules: {
-        toolbar: [
-          ["bold", "italic", "underline", "strike"],
-          ["blockquote", "code-block"],
-          [{ header: 1 }, { header: 2 }],
-          [{ list: "ordered" }, { list: "bullet" }],
-          [{ script: "sub" }, { script: "super" }],
-          [{ indent: "-1" }, { indent: "+1" }],
-          [{ direction: "rtl" }],
-          [{ size: ["small", false, "large", "huge"] }],
-          [{ header: [1, 2, 3, 4, 5, 6, false] }],
-          [{ font: [] }],
-          [{ color: [] }, { background: [] }],
-          [{ align: [] }],
-          ["clean"],
-        ],
+        toolbar: [["bold", "italic", "underline", "strike"], ["clean"]],
         syntax: {
           highlight: (text) => hljs.highlightAuto(text).value,
         },
